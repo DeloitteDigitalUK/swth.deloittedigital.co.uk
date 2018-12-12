3f0d3c92b049d22605057f4e9bee8482742c6bb6
Matt Dell <mattdell29@gmail.com>
2018-12-12 17:27:15 +0000

    Add bridge to landing

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -7,24 +7,18 @@ import styles from './Landing.module.scss';
 
 const Landing = () => (
   <div className={styles.landing}>
-    <div className={styles.opacity}>
-      <WidthControl>
-        <div className={styles.logo}>
-          <Logo />
-        </div>
-        <div className={styles.oval}>
-          <img
-            alt="circle with shining light"
-            src={require('./images/oval.png')}
-          />
-          <h1 className={styles.h1}>South West Technology Hub</h1>
-          <h2 className={styles.h2}>
-            Delivered by <span className={styles.white}>Deloitte</span> and{' '}
-            <span className={styles.white}>Deloitte Digital</span>
-          </h2>
-        </div>
-      </WidthControl>
-    </div>
+    <WidthControl>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
+      <div className={styles.oval}>
+        <h1 className={styles.h1}>South West Technology Hub</h1>
+        <h2 className={styles.h2}>
+          Delivered by <span className={styles.white}>Deloitte</span> and{' '}
+          <span className={styles.white}>Deloitte Digital</span>
+        </h2>
+      </div>
+    </WidthControl>
   </div>
 );
 
